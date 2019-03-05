ff80452ec3fa6912e12bc0e2c2083d547b00cda3
ndt <nguyenduytamgithub@gmail.com>
2019-03-05 06:05:24 +0000

    update thu muc moi

--- code/read.py
+++ code/read.py
@@ -3,15 +3,15 @@ from PIL import ImageFont
 from PIL import ImageDraw
 import csv
 i = 1
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 70)
-with open('card.csv', 'r') as csvFile:
+font = ImageFont.truetype("../font/DejaVuSansMono.ttf", 70)
+with open('../input/card.csv', 'r') as csvFile:
     reader = csv.reader(csvFile)#decode("utf-8-sig").encode("utf-8".splitlines())
     for row in reader:
 	for e in row:
-		img = Image.open("khung_dichvu.png") 
+		img = Image.open("../image_input/image.png") 
 		draw = ImageDraw.Draw(img)
 		draw.text((70, 250),e,(0,0,0),font=font) 
-		img.save('image/card' + str(i) + '.jpg') 				
+		img.save('../output/card' + str(i) + '.jpg') 				
 		i = i + 1
 		print(e)
 csvFile.close()
